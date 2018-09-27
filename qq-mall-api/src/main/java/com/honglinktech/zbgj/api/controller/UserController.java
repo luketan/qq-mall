50e9de1666fed8eb11c56cdce6e867fc0b725fca
luke <361681251@qq.com>
2018-09-27 23:44:56 +0800

    fix bug123

--- qq-mall-api/src/main/java/com/honglinktech/zbgj/api/controller/UserController.java
+++ qq-mall-api/src/main/java/com/honglinktech/zbgj/api/controller/UserController.java
@@ -39,6 +39,8 @@ import java.util.Map;
 @RestController
 @RequestMapping("/user/api")
 public class UserController extends BaseApiController {
+
+
 	@Resource
 	private UserService userService;
 	@Resource
